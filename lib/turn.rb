a6e127e317e442da5e72d850ac9b2e2d333e196d
Kristen Warnquist <kristen.warnquist@gmail.com>
2017-09-19 18:12:05 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,46 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+    if index < 0 || index > 8
+      false
+    elsif position_taken?(board, index)
+      false
+    else true
+    end
+end
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    true
+  elsif board[index] == " "
+    false
+  elsif board[index] == nil
+    false
+  else false
+  end
+end
+
+def input_to_index(user_input)
+  index = user_input.to_i - 1
+end
+
+def move(board, index, character = "X")
+  board[index] = character
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  input_to_index(user_input)
+  until valid_move? = true
+    puts "invalid"
+    
+  end 
+    puts 
+end
